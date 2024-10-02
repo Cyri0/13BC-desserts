1af51285599bbd7759064377f0e818407857c01b
Cyri0 <nemestamas94@gmail.com>
2024-10-02 11:53:04 +0200

    update cart

--- desserts-frontend/src/components/Cart.tsx
+++ desserts-frontend/src/components/Cart.tsx
@@ -1,4 +1,4 @@
-import { useContext } from "react"
+import { useContext, useEffect, useState } from "react"
 import CartItem from "./CartItem"
 import { CartContext } from "../pages/Home"
 
@@ -7,13 +7,39 @@ export type CartItemType = {
     price: number
 }
 
+export type VisibleCartType = {
+    item: CartItemType,
+    quantity: number
+}
+
 const Cart = () => {
     const cartCtx = useContext(CartContext)
     if(!cartCtx){
         throw new Error("Something is wrong, I can feel it")
     }
     const {cart} = cartCtx
-    
+
+    const [visibleCart, setVisibleCart] = useState<VisibleCartType[]>([])
+
+    useEffect(()=>{
+        let unique:string[] = []
+        cart.forEach(cartItem => {
+            if(!unique.includes(cartItem.name)){
+                unique.push(cartItem.name);
+            }
+        })
+        let tempCart = [] // TODO!!!
+        unique.forEach(dessert_name => {
+            let counter = 0
+            cart.forEach(cartItem => {
+                if(cartItem.name === dessert_name){
+                    counter++;
+                }
+            })
+        })
+        
+    }, [cartCtx])
+
   return (
     <aside className="cart">
         <h2>Your Cart ({cart.length})</h2>
